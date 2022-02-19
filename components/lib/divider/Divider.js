10f998d4a38e387c53e7059928caa7cb455a52e9
Cagatay Civici <cagatay.civici@gmail.com>
2022-02-19 14:36:36 +0300

    Converted Divider to functional component

--- components/lib/divider/Divider.js
+++ components/lib/divider/Divider.js
@@ -2,48 +2,39 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { classNames } from '../utils/Utils';
 
-export class Divider extends Component {
+export const Divider = (props) => {
+    const isHorizontal = props.layout === 'horizontal';
+    const isVertical = props.layout === 'vertical';
+    const dividerClassName = classNames(`p-divider p-component p-divider-${props.layout} p-divider-${props.type}`, {
+        'p-divider-left': isHorizontal && (!props.align || props.align === 'left'),
+        'p-divider-right': isHorizontal && props.align === 'right',
+        'p-divider-center': (isHorizontal && props.align === 'center') || (isVertical && (!props.align || props.align === 'center')),
+        'p-divider-top': isVertical && props.align === 'top',
+        'p-divider-bottom': isVertical && props.align === 'bottom',
+    }, props.className);
 
-    static defaultProps = {
-        align: null,
-        layout: 'horizontal',
-        type: 'solid',
-        style: null,
-        className: null
-    }
-
-    static propTypes = {
-        align: PropTypes.string,
-        layout: PropTypes.string,
-        type: PropTypes.string,
-        style: PropTypes.object,
-        className: PropTypes.string
-    };
-
-    get isHorizontal() {
-        return this.props.layout === 'horizontal';
-    }
-
-    get isVertical() {
-        return this.props.layout === 'vertical';
-    }
-
-    render() {
-        const dividerClassName = classNames(`p-divider p-component p-divider-${this.props.layout} p-divider-${this.props.type}`, {
-            'p-divider-left': this.isHorizontal && (!this.props.align || this.props.align === 'left'),
-            'p-divider-right': this.isHorizontal && this.props.align === 'right',
-            'p-divider-center': (this.isHorizontal && this.props.align === 'center') || (this.isVertical && (!this.props.align || this.props.align === 'center')),
-            'p-divider-top': this.isVertical && this.props.align === 'top',
-            'p-divider-bottom': this.isVertical && this.props.align === 'bottom',
-        }, this.props.className);
-
-        return (
-            <div className={dividerClassName} style={this.props.style} role="separator">
-                <div className="p-divider-content">
-                    {this.props.children}
-                </div>
+    return (
+        <div className={dividerClassName} style={props.style} role="separator">
+            <div className="p-divider-content">
+                {props.children}
             </div>
+        </div>
 
-        );
-    }
+    );
 }
+
+Divider.defaultProps = {
+    align: null,
+    layout: 'horizontal',
+    type: 'solid',
+    style: null,
+    className: null
+}
+
+Divider.propTypes = {
+    align: PropTypes.string,
+    layout: PropTypes.string,
+    type: PropTypes.string,
+    style: PropTypes.object,
+    className: PropTypes.string
+};
