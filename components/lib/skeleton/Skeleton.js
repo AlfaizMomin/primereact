690b4256eaa9998cf80105d80f82caec2387c64f
yigitfindikli <yigitfindikli@gmail.com>
2022-02-20 11:11:55 +0300

    Converted Skeleton to a functional component

--- components/lib/skeleton/Skeleton.js
+++ components/lib/skeleton/Skeleton.js
@@ -1,48 +1,44 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { classNames } from '../utils/Utils';
 
-export class Skeleton extends Component {
-
-    static defaultProps = {
-        shape: 'rectangle',
-        size: null,
-        width: '100%',
-        height: '1rem',
-        borderRadius: null,
-        animation: 'wave',
-        style: null,
-        className: null
-    }
-
-    static propTypes = {
-        shape: PropTypes.string,
-        size: PropTypes.string,
-        width: PropTypes.string,
-        height: PropTypes.string,
-        borderRadius: PropTypes.string,
-        animation: PropTypes.string,
-        style: PropTypes.object,
-        className: PropTypes.string
-    };
-
-    skeletonStyle() {
-        if (this.props.size)
-            return { width: this.props.size, height: this.props.size, borderRadius: this.props.borderRadius };
+export const Skeleton = memo((props) => {
+    const skeletonStyle = () => {
+        if (props.size)
+            return { width: props.size, height: props.size, borderRadius: props.borderRadius };
         else
-            return { width: this.props.width, height: this.props.height, borderRadius: this.props.borderRadius };
+            return { width: props.width, height: props.height, borderRadius: props.borderRadius };
     }
 
-    render() {
-        const skeletonClassName = classNames('p-skeleton p-component', {
-            'p-skeleton-circle': this.props.shape === 'circle',
-            'p-skeleton-none': this.props.animation === 'none'
-        }, this.props.className);
+    const skeletonClassName = classNames('p-skeleton p-component', {
+        'p-skeleton-circle': props.shape === 'circle',
+        'p-skeleton-none': props.animation === 'none'
+    }, props.className);
+    const style = skeletonStyle();
 
-        const style = this.skeletonStyle();
+    return (
+        <div style={style} className={skeletonClassName}></div>
+    );
+})
 
-        return (
-            <div style={style} className={skeletonClassName}></div>
-        );
-    }
+Skeleton.defaultProps = {
+    shape: 'rectangle',
+    size: null,
+    width: '100%',
+    height: '1rem',
+    borderRadius: null,
+    animation: 'wave',
+    style: null,
+    className: null
 }
+
+Skeleton.propTypes = {
+    shape: PropTypes.string,
+    size: PropTypes.string,
+    width: PropTypes.string,
+    height: PropTypes.string,
+    borderRadius: PropTypes.string,
+    animation: PropTypes.string,
+    style: PropTypes.object,
+    className: PropTypes.string
+};
