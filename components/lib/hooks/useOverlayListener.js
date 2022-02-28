ba214e50a751daabcc5d1fa5238e218cc3545290
mertsincan <sincan.mert@gmail.com>
2022-02-28 02:03:40 +0000

    New 'useOverlayListener' hook

---
+++ components/lib/hooks/useOverlayListener.js
@@ -0,0 +1,59 @@
+import { useEffect, useRef } from 'react';
+import { DomHandler } from '../utils/Utils';
+import { useEventListener } from './useEventListener';
+import { useResizeListener } from './useResizeListener';
+import { useOverlayScrollListener } from './useOverlayScrollListener';
+
+export const useOverlayListener = ({ target, overlay, listener, when = true }) => {
+    const targetRef = useRef(null);
+    const overlayRef = useRef(null);
+
+    const [bindDocumentClick, unbindDocumentClick] = useEventListener({ type: 'click', listener: event => {
+        isOutsideClicked(event) && listener && listener(event, 'outside');
+    }});
+    const [bindWindowResize, unbindWindowResize] = useResizeListener({ listener: event => {
+        !DomHandler.isTouchDevice() && listener && listener(event, 'resize');
+    }});
+    const [bindOverlayScroll, unbindOverlayScroll] = useOverlayScrollListener({ target: targetRef, listener: event => {
+        listener && listener(event, 'scroll');
+    }});
+
+    const isOutsideClicked = (event) => {
+        return targetRef.current && !(targetRef.current.isSameNode(event.target) || targetRef.current.contains(event.target)
+            || (overlayRef.current && overlayRef.current.contains(event.target)));
+    }
+
+    const bind = () => {
+        bindDocumentClick();
+        bindWindowResize();
+        bindOverlayScroll();
+    }
+
+    const unbind = () => {
+        unbindDocumentClick();
+        unbindWindowResize();
+        unbindOverlayScroll();
+    }
+
+    useEffect(() => {
+        if (when) {
+            targetRef.current = DomHandler.getTargetElement(target);
+            overlayRef.current = DomHandler.getTargetElement(overlay);
+
+            return () => {
+                unbind();
+            }
+        }
+        else {
+            unbind();
+            targetRef.current = overlayRef.current = null;
+        }
+    }, [target, overlay, when]);
+
+    useEffect(() => {
+        unbind();
+        when && bind();
+    }, [when]);
+
+    return [bind, unbind];
+}
