55a00e6c0432c983ea337d044db560c3e4fa3d51
mcandu <dundarmehmetcan@gmail.com>
2022-03-01 22:11:27 +0300

    Converted ProgressSpinner to functional component

--- components/lib/progressspinner/ProgressSpinner.js
+++ components/lib/progressspinner/ProgressSpinner.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { classNames } from '../utils/Utils';
 
-export class ProgressSpinner extends Component {
+export const ProgressSpinner = (props) => {
 
-    static defaultProps = {
-        id: null,
-        style: null,
-        className: null,
-        strokeWidth: "2",
-        fill: "none",
-        animationDuration: "2s"
-    };
+    let spinnerClass = classNames('p-progress-spinner', props.className);
 
-    static propTypes = {
-        id: PropTypes.string,
-        style: PropTypes.object,
-        className: PropTypes.string,
-        strokeWidth: PropTypes.string,
-        fill: PropTypes.string,
-        animationDuration: PropTypes.string
-    };
-
-    render() {
-        let spinnerClass = classNames('p-progress-spinner', this.props.className);
-
-        return <div id={this.props.id} style={this.props.style} className={spinnerClass} role="alert" aria-busy>
-            <svg className="p-progress-spinner-svg" viewBox="25 25 50 50" style={{ animationDuration: this.props.animationDuration }}>
-                <circle className="p-progress-spinner-circle" cx="50" cy="50" r="20" fill={this.props.fill}
-                    strokeWidth={this.props.strokeWidth} strokeMiterlimit="10" />
-            </svg>
-        </div>;
-    }
+    return <div id={props.id} style={props.style} className={spinnerClass} role="alert" aria-busy>
+        <svg className="p-progress-spinner-svg" viewBox="25 25 50 50" style={{ animationDuration: props.animationDuration }}>
+            <circle className="p-progress-spinner-circle" cx="50" cy="50" r="20" fill={props.fill}
+                strokeWidth={props.strokeWidth} strokeMiterlimit="10" />
+        </svg>
+    </div>;
 }
+
+ProgressSpinner.defaultProps = {
+    id: null,
+    style: null,
+    className: null,
+    strokeWidth: "2",
+    fill: "none",
+    animationDuration: "2s"
+};
+
+ProgressSpinner.propTypes = {
+    id: PropTypes.string,
+    style: PropTypes.object,
+    className: PropTypes.string,
+    strokeWidth: PropTypes.string,
+    fill: PropTypes.string,
+    animationDuration: PropTypes.string
+};
