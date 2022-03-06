5d5a6d4be03ef165bb50353f04378cb0cf6941ad
yigitfindikli <yigitfindikli@gmail.com>
2022-03-06 16:38:54 +0300

    Converted TimeLine to functional component

--- components/lib/timeline/Timeline.js
+++ components/lib/timeline/Timeline.js
@@ -2,47 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ObjectUtils, classNames } from '../utils/Utils';
 
-export class Timeline extends Component {
-
-    static defaultProps = {
-        id: null,
-        value: null,
-        align: 'left',
-        layout: 'vertical',
-        dataKey: null,
-        className: null,
-        style: null,
-        opposite: null,
-        marker: null,
-        content: null
-    };
-
-    static propTypes = {
-        id: PropTypes.string,
-        value: PropTypes.array,
-        align: PropTypes.string,
-        layout: PropTypes.string,
-        dataKey: PropTypes.string,
-        className: PropTypes.string,
-        style: PropTypes.object,
-        opposite: PropTypes.any,
-        marker: PropTypes.any,
-        content: PropTypes.any
-    };
-
-    getKey(item, index) {
-        return this.props.dataKey ? ObjectUtils.resolveFieldData(item, this.props.dataKey) : `pr_id__${index}`;
+export function Timeline(props) {
+    const getKey = (item, index) => {
+        return props.dataKey ? ObjectUtils.resolveFieldData(item, props.dataKey) : `pr_id__${index}`;
     }
 
-    renderEvents() {
-        return this.props.value && this.props.value.map((item, index) => {
-            const opposite = ObjectUtils.getJSXElement(this.props.opposite, item, index);
-            const marker = ObjectUtils.getJSXElement(this.props.marker, item, index) || <div className="p-timeline-event-marker"></div>;
-            const connector = index !== (this.props.value.length - 1) && <div className="p-timeline-event-connector"></div>;
-            const content = ObjectUtils.getJSXElement(this.props.content, item, index);
+    const useEvents = () => {
+        return props.value && props.value.map((item, index) => {
+            const opposite = ObjectUtils.getJSXElement(props.opposite, item, index);
+            const marker = ObjectUtils.getJSXElement(props.marker, item, index) || <div className="p-timeline-event-marker"></div>;
+            const connector = index !== (props.value.length - 1) && <div className="p-timeline-event-connector"></div>;
+            const content = ObjectUtils.getJSXElement(props.content, item, index);
 
             return (
-                <div key={this.getKey(item, index)} className="p-timeline-event">
+                <div key={getKey(item, index)} className="p-timeline-event">
                     <div className="p-timeline-event-opposite">
                         {opposite}
                     </div>
@@ -58,18 +31,43 @@ export class Timeline extends Component {
         });
     }
 
-    render() {
-        const containerClassName = classNames('p-timeline p-component', {
-            [`p-timeline-${this.props.align}`]: true,
-            [`p-timeline-${this.props.layout}`]: true
-        }, this.props.className);
+    const containerClassName = classNames('p-timeline p-component', {
+        [`p-timeline-${props.align}`]: true,
+        [`p-timeline-${props.layout}`]: true
+    }, props.className);
 
-        const events = this.renderEvents();
+    const events = useEvents();
 
-        return (
-            <div id={this.props.id} className={containerClassName} style={this.props.style}>
-                {events}
-            </div>
-        )
-    }
+    return (
+        <div id={props.id} className={containerClassName} style={props.style}>
+            {events}
+        </div>
+    );
 }
+
+
+Timeline.defaultProps = {
+    id: null,
+    value: null,
+    align: 'left',
+    layout: 'vertical',
+    dataKey: null,
+    className: null,
+    style: null,
+    opposite: null,
+    marker: null,
+    content: null
+};
+
+Timeline.propTypes = {
+    id: PropTypes.string,
+    value: PropTypes.array,
+    align: PropTypes.string,
+    layout: PropTypes.string,
+    dataKey: PropTypes.string,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    opposite: PropTypes.any,
+    marker: PropTypes.any,
+    content: PropTypes.any
+};
