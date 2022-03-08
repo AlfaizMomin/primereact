b758276ab72067cdf93c90dc96304280d8dfbd6e
yigitfindikli <yigitfindikli@gmail.com>
2022-03-08 14:18:49 +0300

    Converted DataView to functional component

--- components/lib/dataview/DataView.js
+++ components/lib/dataview/DataView.js
@@ -1,211 +1,94 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Paginator } from '../paginator/Paginator';
 import { ObjectUtils, classNames } from '../utils/Utils';
 import { Ripple } from '../ripple/Ripple';
 import { localeOption } from '../api/Api';
+import PrimeReact from '../api/Api';
 
-export class DataViewLayoutOptions extends Component {
+export const DataViewLayoutOptions = (props) => {
 
-    static defaultProps = {
-        id: null,
-        style: null,
-        className: null,
-        layout: null,
-        onChange: null
-    }
-
-    static propTypes = {
-        id: PropTypes.string,
-        style: PropTypes.object,
-        className: PropTypes.string,
-        layout: PropTypes.string,
-        onChange: PropTypes.func.isRequired
-    }
-
-    constructor(props) {
-        super(props);
-        this.changeLayout = this.changeLayout.bind(this);
-    }
-
-    changeLayout(event, layoutMode) {
-        this.props.onChange({
+    const changeLayout = (event, layoutMode) => {
+        props.onChange({
             originalEvent: event,
             value: layoutMode
         });
         event.preventDefault();
     }
+    
+    const className = classNames('p-dataview-layout-options p-selectbutton p-buttonset', props.className);
+    const buttonListClass = classNames('p-button p-button-icon-only', { 'p-highlight': props.layout === 'list' });
+    const buttonGridClass = classNames('p-button p-button-icon-only', { 'p-highlight': props.layout === 'grid' });
 
-    render() {
-        const className = classNames('p-dataview-layout-options p-selectbutton p-buttonset', this.props.className);
-        const buttonListClass = classNames('p-button p-button-icon-only', { 'p-highlight': this.props.layout === 'list' });
-        const buttonGridClass = classNames('p-button p-button-icon-only', { 'p-highlight': this.props.layout === 'grid' });
-
-        return (
-            <div id={this.props.id} style={this.props.style} className={className}>
-                <button type="button" className={buttonListClass} onClick={(event) => this.changeLayout(event, 'list')}>
-                    <i className="pi pi-bars"></i>
-                    <Ripple />
-                </button>
-                <button type="button" className={buttonGridClass} onClick={(event) => this.changeLayout(event, 'grid')}>
-                    <i className="pi pi-th-large"></i>
-                    <Ripple />
-                </button>
-            </div>
-        );
-    }
+    return (
+        <div id={props.id} style={props.style} className={className}>
+            <button type="button" className={buttonListClass} onClick={(event) => changeLayout(event, 'list')}>
+                <i className="pi pi-bars"></i>
+                <Ripple />
+            </button>
+            <button type="button" className={buttonGridClass} onClick={(event) => changeLayout(event, 'grid')}>
+                <i className="pi pi-th-large"></i>
+                <Ripple />
+            </button>
+        </div>
+    );
 }
 
-class DataViewItem extends Component {
-
-    static defaultProps = {
-        template: null,
-        item: null,
-        layout: null
-    }
-
-    static propTypes = {
-        template: PropTypes.func,
-        item: PropTypes.any,
-        layout: PropTypes.string
-    }
-
-    render() {
-        return this.props.template(this.props.item, this.props.layout);
-    }
-
+export const DataViewItem = (props) => {
+    return props.template(props.item, props.layout);
 }
 
-export class DataView extends Component {
+export const DataView = (props) => {
 
-    static defaultProps = {
-        id: null,
-        header: null,
-        footer: null,
-        value: null,
-        layout: 'list',
-        dataKey: null,
-        rows: null,
-        first: 0,
-        totalRecords: null,
-        paginator: false,
-        paginatorPosition: 'bottom',
-        alwaysShowPaginator: true,
-        paginatorClassName: null,
-        paginatorTemplate: 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown',
-        paginatorLeft: null,
-        paginatorRight: null,
-        paginatorDropdownAppendTo: null,
-        pageLinkSize: 5,
-        rowsPerPageOptions: null,
-        currentPageReportTemplate: '({currentPage} of {totalPages})',
-        emptyMessage: null,
-        sortField: null,
-        sortOrder: null,
-        style: null,
-        className: null,
-        lazy: false,
-        loading: false,
-        loadingIcon: 'pi pi-spinner',
-        itemTemplate: null,
-        onPage: null
+    const [first, setFirst] = useState(!props.onPage ? props.first : null);
+    const [rows, setRows] = useState(!props.onPage ? props.rows : null);
+
+    const getItemRenderKey = (value) => {
+        return props.dataKey ? ObjectUtils.resolveFieldData(value, props.dataKey) : null;
     }
 
-    static propTypes = {
-        id: PropTypes.string,
-        header: PropTypes.any,
-        footer: PropTypes.any,
-        value: PropTypes.array,
-        layout: PropTypes.string,
-        dataKey: PropTypes.string,
-        rows: PropTypes.number,
-        first: PropTypes.number,
-        totalRecords: PropTypes.number,
-        paginator: PropTypes.bool,
-        paginatorPosition: PropTypes.string,
-        alwaysShowPaginator: PropTypes.bool,
-        paginatorClassName: PropTypes.string,
-        paginatorTemplate: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-        paginatorLeft: PropTypes.any,
-        paginatorRight: PropTypes.any,
-        paginatorDropdownAppendTo: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-        pageLinkSize: PropTypes.number,
-        rowsPerPageOptions: PropTypes.array,
-        currentPageReportTemplate: PropTypes.string,
-        emptyMessage: PropTypes.string,
-        sortField: PropTypes.string,
-        sortOrder: PropTypes.number,
-        style: PropTypes.object,
-        className: PropTypes.string,
-        lazy: PropTypes.bool,
-        loading: PropTypes.bool,
-        loadingIcon: PropTypes.string,
-        itemTemplate: PropTypes.func.isRequired,
-        onPage: PropTypes.func
-    }
-
-    constructor(props) {
-        super(props);
-
-        if (!this.props.onPage) {
-            this.state = {
-                first: this.props.first,
-                rows: this.props.rows
-            };
-        }
-
-        this.sortChange = false;
-        this.onPageChange = this.onPageChange.bind(this)
-    }
-
-    getItemRenderKey(value) {
-        return this.props.dataKey ? ObjectUtils.resolveFieldData(value, this.props.dataKey) : null;
-    }
-
-    getTotalRecords() {
-        if (this.props.totalRecords)
-            return this.props.totalRecords;
+    const getTotalRecords = () => {
+        if (props.totalRecords)
+            return props.totalRecords;
         else
-            return this.props.value ? this.props.value.length : 0;
+            return props.value ? props.value.length : 0;
     }
 
-    createPaginator(position) {
-        const className = classNames('p-paginator-' + position, this.props.paginatorClassName);
-        const first = this.props.onPage ? this.props.first : this.state.first;
-        const rows = this.props.onPage ? this.props.rows : this.state.rows;
-        const totalRecords = this.getTotalRecords();
+    const createPaginator = (position) => {
+        const className = classNames('p-paginator-' + position, props.paginatorClassName);
+        const _first = props.onPage ? props.first : first;
+        const _rows = props.onPage ? props.rows : rows;
+        const totalRecords = getTotalRecords();
 
         return (
-            <Paginator first={first} rows={rows} pageLinkSize={this.props.pageLinkSize} className={className} onPageChange={this.onPageChange} template={this.props.paginatorTemplate}
-                totalRecords={totalRecords} rowsPerPageOptions={this.props.rowsPerPageOptions} currentPageReportTemplate={this.props.currentPageReportTemplate}
-                leftContent={this.props.paginatorLeft} rightContent={this.props.paginatorRight} alwaysShow={this.props.alwaysShowPaginator} dropdownAppendTo={this.props.paginatorDropdownAppendTo} />
+            <Paginator first={_first} rows={_rows} pageLinkSize={props.pageLinkSize} className={className} onPageChange={onPageChange} template={props.paginatorTemplate}
+                totalRecords={totalRecords} rowsPerPageOptions={props.rowsPerPageOptions} currentPageReportTemplate={props.currentPageReportTemplate}
+                leftContent={props.paginatorLeft} rightContent={props.paginatorRight} alwaysShow={props.alwaysShowPaginator} dropdownAppendTo={props.paginatorDropdownAppendTo} />
         );
     }
 
-    onPageChange(event) {
-        if (this.props.onPage) {
-            this.props.onPage(event);
+    const onPageChange = (event) => {
+        if (props.onPage) {
+            props.onPage(event);
         }
         else {
-            this.setState({
-                first: event.first,
-                rows: event.rows
-            });
+            setFirst(event.first)
+            setRows(event.rows);
         }
     }
 
-    isEmpty() {
-        return (!this.props.value || this.props.value.length === 0);
+    const isEmpty = () => {
+        return (!props.value || props.value.length === 0);
     }
 
-    sort() {
-        if (this.props.value) {
-            const value = [...this.props.value];
+    const sort = () => {
+        if (props.value) {
+            const value = [...props.value];
 
             value.sort((data1, data2) => {
-                let value1 = ObjectUtils.resolveFieldData(data1, this.props.sortField);
-                let value2 = ObjectUtils.resolveFieldData(data2, this.props.sortField);
-                return ObjectUtils.sort(value1, value2, this.props.sortOrder, PrimeReact.locale);
+                let value1 = ObjectUtils.resolveFieldData(data1, props.sortField);
+                let value2 = ObjectUtils.resolveFieldData(data2, props.sortField);
+                return ObjectUtils.sort(value1, value2, props.sortOrder, PrimeReact.locale);
             });
 
             return value;
@@ -215,9 +98,9 @@ export class DataView extends Component {
         }
     }
 
-    renderLoader() {
-        if (this.props.loading) {
-            let iconClassName = classNames('p-dataview-loading-icon pi-spin', this.props.loadingIcon);
+    const useLoader = () => {
+        if (props.loading) {
+            let iconClassName = classNames('p-dataview-loading-icon pi-spin', props.loadingIcon);
 
             return (
                 <div className="p-dataview-loading-overlay p-component-overlay">
@@ -229,25 +112,26 @@ export class DataView extends Component {
         return null;
     }
 
-    renderTopPaginator() {
-        if (this.props.paginator && (this.props.paginatorPosition !== 'bottom' || this.props.paginatorPosition === 'both')) {
-            return this.createPaginator('top');
+    const useTopPaginator = () => {
+        if (props.paginator && (props.paginatorPosition !== 'bottom' || props.paginatorPosition === 'both')) {
+            return createPaginator('top');
         }
 
         return null;
     }
 
-    renderBottomPaginator() {
-        if (this.props.paginator && (this.props.paginatorPosition !== 'top' || this.props.paginatorPosition === 'both')) {
-            return this.createPaginator('bottom');
+    
+    const useBottomPaginator = () => {
+        if (props.paginator && (props.paginatorPosition !== 'top' || props.paginatorPosition === 'both')) {
+            return createPaginator('bottom');
         }
 
         return null;
     }
 
-    renderEmptyMessage() {
-        if (!this.props.loading) {
-            const content = this.props.emptyMessage || localeOption('emptyMessage');
+    const useEmptyMessage = () => {
+        if (!props.loading) {
+            const content = props.emptyMessage || localeOption('emptyMessage');
 
             return <div className="p-col-12 col-12 p-dataview-emptymessage">{content}</div>;
         }
@@ -255,53 +139,52 @@ export class DataView extends Component {
         return null;
     }
 
-    renderHeader() {
-        if (this.props.header) {
-            return <div className="p-dataview-header">{this.props.header}</div>;
+    const useHeader = () => {
+        if (props.header) {
+            return <div className="p-dataview-header">{props.header}</div>;
         }
 
         return null;
     }
 
-    renderFooter() {
-        if (this.props.footer) {
-            return <div className="p-dataview-footer"> {this.props.footer}</div>;
+    const useFooter = () => {
+        if (props.footer) {
+            return <div className="p-dataview-footer"> {props.footer}</div>;
         }
 
         return null;
     }
 
-    renderItems(value) {
+    const useItems = (value) => {
         if (value && value.length) {
-            if (this.props.paginator) {
-                const rows = this.props.onPage ? this.props.rows : this.state.rows;
-                const first = this.props.lazy ? 0 : this.props.onPage ? this.props.first : this.state.first;
-                const totalRecords = this.getTotalRecords();
-                const last = Math.min(rows + first, totalRecords);
+            if (props.paginator) {
+                const _rows = props.onPage ? props.rows : rows;
+                const _first = props.lazy ? 0 : props.onPage ? props.first : first;
+                const totalRecords = getTotalRecords();
+                const last = Math.min(_rows + _first, totalRecords);
                 let items = [];
 
                 for (let i = first; i < last; i++) {
                     const val = value[i];
-                    val && items.push(<DataViewItem key={this.getItemRenderKey(value) || i} template={this.props.itemTemplate} layout={this.props.layout} item={val} />);
+                    val && items.push(<DataViewItem key={getItemRenderKey(value) || i} template={props.itemTemplate} layout={props.layout} item={val} />);
                 }
-
                 return items;
             }
             else {
                 return (
                     value.map((item, index) => {
-                        return <DataViewItem key={this.getItemRenderKey(item) || index} template={this.props.itemTemplate} layout={this.props.layout} item={item} />
+                        return <DataViewItem key={getItemRenderKey(item) || index} template={props.itemTemplate} layout={props.layout} item={item} />
                     })
                 );
             }
         }
         else {
-            return this.renderEmptyMessage();
+            return useEmptyMessage();
         }
     }
 
-    renderContent(value) {
-        const items = this.renderItems(value);
+    const useContent = (value) => {
+        const items = useItems(value);
 
         return (
             <div className="p-dataview-content">
@@ -312,38 +195,129 @@ export class DataView extends Component {
         );
     }
 
-    processData() {
-        let data = this.props.value;
+    const processData = () => {
+        let data = props.value;
 
         if (data && data.length) {
-            if (this.props.sortField) {
-                data = this.sort();
+            if (props.sortField) {
+                data = sort();
             }
         }
 
         return data;
     }
 
-    render() {
-        const value = this.processData();
-        const className = classNames('p-dataview p-component', { 'p-dataview-list': (this.props.layout === 'list'), 'p-dataview-grid': (this.props.layout === 'grid'), 'p-dataview-loading': this.props.loading }, this.props.className);
-        const loader = this.renderLoader();
-        const topPaginator = this.renderTopPaginator();
-        const bottomPaginator = this.renderBottomPaginator();
-        const header = this.renderHeader();
-        const footer = this.renderFooter();
-        const content = this.renderContent(value);
+    const value = processData();
+    const className = classNames('p-dataview p-component', { 'p-dataview-list': (props.layout === 'list'), 'p-dataview-grid': (props.layout === 'grid'), 'p-dataview-loading': props.loading }, props.className);
+    const loader = useLoader();
+    const topPaginator = useTopPaginator();
+    const bottomPaginator = useBottomPaginator();
+    const header = useHeader();
+    const footer = useFooter();
+    const content = useContent(value);
 
-        return (
-            <div id={this.props.id} style={this.props.style} className={className}>
-                {loader}
-                {header}
-                {topPaginator}
-                {content}
-                {bottomPaginator}
-                {footer}
-            </div>
-        );
-    }
+    return (
+        <div id={props.id} style={props.style} className={className}>
+            {loader}
+            {header}
+            {topPaginator}
+            {content}
+            {bottomPaginator}
+            {footer}
+        </div>
+    );
+}
 
+DataView.defaultProps = {
+    id: null,
+    header: null,
+    footer: null,
+    value: null,
+    layout: 'list',
+    dataKey: null,
+    rows: null,
+    first: 0,
+    totalRecords: null,
+    paginator: false,
+    paginatorPosition: 'bottom',
+    alwaysShowPaginator: true,
+    paginatorClassName: null,
+    paginatorTemplate: 'FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown',
+    paginatorLeft: null,
+    paginatorRight: null,
+    paginatorDropdownAppendTo: null,
+    pageLinkSize: 5,
+    rowsPerPageOptions: null,
+    currentPageReportTemplate: '({currentPage} of {totalPages})',
+    emptyMessage: null,
+    sortField: null,
+    sortOrder: null,
+    style: null,
+    className: null,
+    lazy: false,
+    loading: false,
+    loadingIcon: 'pi pi-spinner',
+    itemTemplate: null,
+    onPage: null
+}
+
+DataView.propTypes = {
+    id: PropTypes.string,
+    header: PropTypes.any,
+    footer: PropTypes.any,
+    value: PropTypes.array,
+    layout: PropTypes.string,
+    dataKey: PropTypes.string,
+    rows: PropTypes.number,
+    first: PropTypes.number,
+    totalRecords: PropTypes.number,
+    paginator: PropTypes.bool,
+    paginatorPosition: PropTypes.string,
+    alwaysShowPaginator: PropTypes.bool,
+    paginatorClassName: PropTypes.string,
+    paginatorTemplate: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    paginatorLeft: PropTypes.any,
+    paginatorRight: PropTypes.any,
+    paginatorDropdownAppendTo: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    pageLinkSize: PropTypes.number,
+    rowsPerPageOptions: PropTypes.array,
+    currentPageReportTemplate: PropTypes.string,
+    emptyMessage: PropTypes.string,
+    sortField: PropTypes.string,
+    sortOrder: PropTypes.number,
+    style: PropTypes.object,
+    className: PropTypes.string,
+    lazy: PropTypes.bool,
+    loading: PropTypes.bool,
+    loadingIcon: PropTypes.string,
+    itemTemplate: PropTypes.func.isRequired,
+    onPage: PropTypes.func
+}
+
+DataViewItem.defaultProps = {
+    template: null,
+    item: null,
+    layout: null
+}
+
+DataViewItem.propTypes = {
+    template: PropTypes.func,
+    item: PropTypes.any,
+    layout: PropTypes.string
+}
+
+DataViewLayoutOptions.defaultProps = {
+    id: null,
+    style: null,
+    className: null,
+    layout: null,
+    onChange: null
+}
+
+DataViewLayoutOptions.propTypes = {
+    id: PropTypes.string,
+    style: PropTypes.object,
+    className: PropTypes.string,
+    layout: PropTypes.string,
+    onChange: PropTypes.func.isRequired
 }
