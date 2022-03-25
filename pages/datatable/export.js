2d03619303fe0fabb60e8be482a9bcfb484bb9b0
yigitfindikli <yigitfindikli@gmail.com>
2022-03-25 14:13:59 +0300

    Update export.js

--- pages/datatable/export.js
+++ pages/datatable/export.js
@@ -100,11 +100,11 @@ const DataTableExportDemo = () => {
 
     const exportPdf = () => {
         import("jspdf").then((jsPDF) => {
-          import("jspdf-autotable").then(() => {
-            const doc = new jsPDF.default(0, 0);
-            doc.autoTable(exportColumns, products);
-            doc.save("products.pdf");
-          });
+            import("jspdf-autotable").then(() => {
+                const doc = new jsPDF.default(0, 0);
+                doc.autoTable(exportColumns, products);
+                doc.save("products.pdf");
+            });
         });
     };
 
@@ -113,8 +113,8 @@ const DataTableExportDemo = () => {
             const worksheet = xlsx.utils.json_to_sheet(products);
             const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
             const excelBuffer = xlsx.write(workbook, {
-            bookType: "xlsx",
-            type: "array"
+                bookType: "xlsx",
+                type: "array"
             });
             saveAsExcelFile(excelBuffer, "products");
         });
@@ -123,17 +123,17 @@ const DataTableExportDemo = () => {
     const saveAsExcelFile = (buffer, fileName) => {
         import("file-saver").then((module) => {
             if (module && module.default) {
-            let EXCEL_TYPE =
-                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-            let EXCEL_EXTENSION = ".xlsx";
-            const data = new Blob([buffer], {
-                type: EXCEL_TYPE
-            });
+                let EXCEL_TYPE =
+                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+                let EXCEL_EXTENSION = ".xlsx";
+                const data = new Blob([buffer], {
+                    type: EXCEL_TYPE
+                });
 
-            module.default.saveAs(
-                data,
-                fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
-            );
+                module.default.saveAs(
+                    data,
+                    fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+                );
             }
         });
     };
